131bce3d535200ff4804e70f96f0e93c4beded96
Keegan Morrow <Korrow@gmail.com>
2020-08-16 15:25:10 -0700

    Literal: Move some function defs to cpp
    Where they should have been all along!

--- src/literal.hpp
+++ src/literal.hpp
@@ -47,56 +47,9 @@ public:
 
     bool operator!=(const LiteralVal &other) const { return m_value == other.m_value; }
 
-    [[nodiscard]] LiteralValType type() const
-    {
-        if (std::holds_alternative<std::string>(m_value))
-        {
-            return LiteralValType::String;
-        }
-        if (std::holds_alternative<bool>(m_value))
-        {
-            return LiteralValType::Bool;
-        }
-        if (std::holds_alternative<double>(m_value))
-        {
-            return LiteralValType::Number;
-        }
-        if (std::holds_alternative<NilLiteral>(m_value))
-        {
-            return LiteralValType::Nil;
-        }
-        spdlog::error("LiteralVal type() requested but is invalid, index is {}", m_value.index());
-        // TODO: Use better exception
-        throw(std::exception());
-    }
+    [[nodiscard]] LiteralValType type() const;
 
-    [[nodiscard]] std::string repr() const
-    {
-        if (const auto *pstr(std::get_if<std::string>(&m_value)); pstr)
-        {
-            return *pstr;
-        }
-        if (const auto *pdoub(std::get_if<double>(&m_value)); pdoub)
-        {
-            return std::to_string(*pdoub);
-        }
-        if (const auto *pbool(std::get_if<bool>(&m_value)); pbool)
-        {
-            if (*pbool)
-            {
-                return "true";
-            }
-            return "false";
-        }
-        if (const auto *pnil(std::get_if<NilLiteral>(&m_value)); pnil)
-        {
-            return "nil";
-        }
-
-        spdlog::error("LiteralVal repr() requested but is invalid, index is this is {:p}",
-                      (void *)this);
-        throw(std::exception());
-    }
+    [[nodiscard]] std::string repr() const;
     template <typename T>
     friend T getLiteral(LiteralVal &val);
 
